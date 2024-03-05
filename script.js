43603f75909db89cd768769d231eae0e4ab06b3b
luisaviveros <luisaviveros73@gmail.com>
2024-03-05 09:00:34 -0500

    tarea11js

---
+++ script.js
@@ -0,0 +1,7 @@
+const nombre = prompt("Cual es tu nombre?");
+const apellido = prompt("Cual es tu apellido?");
+const AñoDeNacimiento= parseInt(prompt("Cual es tu año de nacimiento?"));
+const Calcularedad = 2024 - AñoDeNacimiento;
+const diadenaciemiento= prompt("Cual es tu dia de nacimiento?");
+const mesdenacimiento= prompt("Cual es tu mes de nacimiento?");
+console.log ("mi nombre es ", nombre, "mi apellido es", apellido, "mi edad es", Calcularedad, " años y naci el", diadenaciemiento, "de", mesdenacimiento, "de", AñoDeNacimiento);
